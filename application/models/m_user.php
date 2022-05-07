3129240bc8e46023e226ea41472acd781d92a469
Taufiiqul Hakim <51141717+taufik230301@users.noreply.github.com>
2022-05-07 09:45:35 +0700

    add image home, visi misi, public page

--- application/models/m_user.php
+++ application/models/m_user.php
@@ -4,10 +4,12 @@ class M_user extends CI_Model
 {
     public function pendaftaran_user($id, $username, $email, $no_telp, $password, $id_user_level, $id_status_verifikasi)
     {
-        $this->db->trans_start();
+       $this->db->trans_start();
+
        $this->db->query("INSERT INTO user(username,password,email ,no_telp ,id_user_level, id_user_detail) VALUES ('$username','$password','$email','$no_telp','$id_user_level','$id')");
        $this->db->query("INSERT INTO user_detail(id_user_detail, id_status_verifikasi) VALUES ('$id','$id_status_verifikasi')");
-        $this->db->trans_complete();
+
+       $this->db->trans_complete();
         if($this->db->trans_status()==true)
             return true;
         else
@@ -16,7 +18,9 @@ class M_user extends CI_Model
 
     public function cek_login($username, $password)
     {
+        
         $hasil=$this->db->query("SELECT * FROM user JOIN user_detail ON user.id_user_detail = user_detail.id_user_detail WHERE username='$username' AND password='$password' ");
         return $hasil;
+        
     }
 }
