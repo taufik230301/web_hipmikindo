d969b8fb6e8ce91361f15d7225d1b00aa69b2026
Taufiiqul Hakim <51141717+taufik230301@users.noreply.github.com>
2022-05-09 09:12:04 +0700

    add serttings modal

--- application/views/admin/components/sidebar.php
+++ application/views/admin/components/sidebar.php
@@ -1,4 +1,4 @@
-<aside class="main-sidebar sidebar-dark-primary elevation-4">
+<aside class="side-bar-custom main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
         <img src="<?= base_url() ?>assets/image/hipmikindo_logo.jpeg" alt="AdminLTE Logo"
