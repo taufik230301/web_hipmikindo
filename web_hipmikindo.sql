d4891d5be9bf321a80ff5645ea10ebd9f7954b6a
taufik230301 <51141717+taufik230301@users.noreply.github.com>
2022-07-25 06:46:00 +0700

    add new sql

--- web_hipmikindo.sql
+++ web_hipmikindo.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 20, 2022 at 03:44 AM
+-- Generation Time: Jul 25, 2022 at 01:44 AM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 7.4.13
 
@@ -54,9 +54,10 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `username`, `password`, `email`, `no_telp`, `id_user_level`, `id_user_detail`, `token`) VALUES
+('06aeebeac840c20b995e71da2e602ae7', 'baru', '123', 'baru@gmail.com', '0812712671', 2, '06aeebeac840c20b995e71da2e602ae7', NULL),
 ('0f49d8519cb0233a78a143460f18d061', 'kresna123', '123', 'malian123@gmail.com', '0812781728', 2, '0f49d8519cb0233a78a143460f18d061', NULL),
-('324cc16f5879fe7c22145281456f2bc8', 'malian', '123', 'malian123@gmail.com', '+62812781728', 2, '324cc16f5879fe7c22145281456f2bc8', NULL),
-('50207cacbc9af45e566eb638f8ed12b2', 'malas', '1234', 'taufiiqul.hakim@binus.ac.id', '0812781728', 2, '50207cacbc9af45e566eb638f8ed12b2', '93989f35fdf51a3e152b5bb627036686'),
+('805970851350129b90d436fc887f1740', 'tes', 'tes', 'taufiiqulhakim23@gmail.com', '+62812781728', 2, '805970851350129b90d436fc887f1740', NULL),
+('9cfe6b2a690f026c5d0531709354ac71', 'tes', '123', 'tespupr@gmail.com', '0812781728', 2, '9cfe6b2a690f026c5d0531709354ac71', NULL),
 ('d41d8cd98f00b204e9800998ecf8427e', 'admin', 'admin', 'admin@gmail.com', '081217812', 1, 'd41d8cd98f00b204e9800998ecf8427e', NULL);
 
 -- --------------------------------------------------------
@@ -69,7 +70,24 @@ CREATE TABLE `user_detail` (
   `id_user_detail` varchar(256) NOT NULL,
   `nama_lengkap` varchar(30) DEFAULT NULL,
   `alamat` text DEFAULT NULL,
-  `logo_usaha` varchar(250) NOT NULL,
+  `nik` varchar(40) DEFAULT NULL,
+  `tempat_lahir` varchar(40) DEFAULT NULL,
+  `tanggal_lahir` date DEFAULT NULL,
+  `penanggung_jawab` varchar(40) DEFAULT NULL,
+  `jenis_usaha` varchar(40) DEFAULT NULL,
+  `nama_usaha` varchar(50) DEFAULT NULL,
+  `jenis_produk_jasa` varchar(40) DEFAULT NULL,
+  `merk_produk_jasa` varchar(40) DEFAULT NULL,
+  `kapasitas_usaha` varchar(40) DEFAULT NULL,
+  `pendapatan` int(100) DEFAULT NULL,
+  `mulai_usaha` date DEFAULT NULL,
+  `jumlah_karyawan` varchar(40) DEFAULT NULL,
+  `mesin_produksi` varchar(40) DEFAULT NULL,
+  `luas_lahan_ruang` varchar(40) DEFAULT NULL,
+  `ijin_dimiliki` varchar(40) DEFAULT NULL,
+  `logo_usaha` varchar(250) DEFAULT NULL,
+  `keluhan_usaha` text DEFAULT NULL,
+  `ktp` varchar(255) DEFAULT NULL,
   `id_status_verifikasi` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -77,11 +95,12 @@ CREATE TABLE `user_detail` (
 -- Dumping data for table `user_detail`
 --
 
-INSERT INTO `user_detail` (`id_user_detail`, `nama_lengkap`, `alamat`, `logo_usaha`, `id_status_verifikasi`) VALUES
-('d41d8cd98f00b204e9800998ecf8427e', NULL, NULL, '', 1),
-('0f49d8519cb0233a78a143460f18d061', 'Kripik Ubi', 'Jl. Sekip', 'f851d1543c51fba6a8498d376f1db9b4.png', 4),
-('50207cacbc9af45e566eb638f8ed12b2', 'Malas Koding', 'Jl.Belanti', 'de419f84840fab46835e738bd233e696.png', 4),
-('324cc16f5879fe7c22145281456f2bc8', NULL, NULL, '', 3);
+INSERT INTO `user_detail` (`id_user_detail`, `nama_lengkap`, `alamat`, `nik`, `tempat_lahir`, `tanggal_lahir`, `penanggung_jawab`, `jenis_usaha`, `nama_usaha`, `jenis_produk_jasa`, `merk_produk_jasa`, `kapasitas_usaha`, `pendapatan`, `mulai_usaha`, `jumlah_karyawan`, `mesin_produksi`, `luas_lahan_ruang`, `ijin_dimiliki`, `logo_usaha`, `keluhan_usaha`, `ktp`, `id_status_verifikasi`) VALUES
+('d41d8cd98f00b204e9800998ecf8427e', NULL, NULL, '', '', '0000-00-00', '', '', NULL, '', '', '', 0, '0000-00-00', '', '', '', '', '', NULL, NULL, 1),
+('0f49d8519cb0233a78a143460f18d061', 'Kripik Ubi', 'Jl. Sekip', '', '', '0000-00-00', '', '', NULL, '', '', '', 0, '0000-00-00', '', '', '', '', 'f851d1543c51fba6a8498d376f1db9b4.png', NULL, NULL, 4),
+('06aeebeac840c20b995e71da2e602ae7', 'Cuciyok', 'Jl. Sekip', '', '', '0000-00-00', '', '', NULL, '', '', '', 0, '0000-00-00', '', '', '', '', '3aa830525fafca3d727df2add62f6484.png', NULL, NULL, 1),
+('9cfe6b2a690f026c5d0531709354ac71', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 1),
+('805970851350129b90d436fc887f1740', 'Taufiiqulhakim 10219021', 'Jl. Sekip', '061827182871812', 'Palembang', '1111-02-23', '2019220028', 'tes', 'Cuciyok', 'tes', 'tes', 'tes', 100, '1291-02-23', '100', 'ye', 'ey', 'eh', 'ed29ec9973639509f70e54f0ca96c32f_logo_usaha.png', 'eh', 'ed29ec9973639509f70e54f0ca96c32f_ktp.png', 1);
 
 -- --------------------------------------------------------
 
