7e5c79019d5313f20a5dae7e31f14fcef022ebc2
taufik230301 <51141717+taufik230301@users.noreply.github.com>
2022-07-29 09:34:50 +0700

    add peta

--- application/views/public_page_detail.php
+++ application/views/public_page_detail.php
@@ -52,7 +52,7 @@
             <nav id="navbar" class="navbar order-last order-lg-0">
                 <ul>
                     <li><a class="nav-link scrollto " href="<?=base_url();?>Web_Public/index">Home</a></li>
-   
+
                 </ul>
                 <i class="bi bi-list mobile-nav-toggle"></i>
             </nav><!-- .navbar -->
@@ -97,6 +97,11 @@
                                 </div>
 
                             </div>
+                            <div class="mt-5">
+                                <iframe src="https://maps.google.com/maps?q=<?=$data_user['alamat']?>&output=embed"
+                                    frameborder="0" style="border:0; width: 100%; height: 290px;"
+                                    allowfullscreen></iframe>
+                            </div>
                             <div class="swiper-pagination"></div>
                         </div>
                     </div>
@@ -119,12 +124,14 @@
                                 <li><strong>Ijin Dimiliki</strong>: <?=$data_user['ijin_dimiliki']?></li>
                             </ul>
                         </div>
+
                         <div class="portfolio-description">
                             <h2>Alamat</h2>
                             <p>
                                 <?=$data_user['alamat']?>
                             </p>
                         </div>
+
                     </div>
 
                 </div>
@@ -140,14 +147,14 @@
             <div class="container">
                 <div class="row">
 
-                    <div class="col-lg-3 col-md-6">
+                    <div class="col-lg-7 col-md-6">
                         <div class="footer-info">
-                            <h3>Scaffold</h3>
+                            <h3>HIPMIKINDO</h3>
                             <p>
-                                A108 Adam Street <br>
-                                NY 535022, USA<br><br>
+                                Jl. Merdeka No.66, Talant Semut, Kec. Bukit Kecil <br>
+                                Kota Palembang, Sumatera Selatan 30136<br><br>
                                 <strong>Phone:</strong> +1 5589 55488 55<br>
-                                <strong>Email:</strong> info@example.com<br>
+                                <strong>Email:</strong> dpd.hipmikindo.sumsel@gmail.com<br>
                             </p>
                             <div class="social-links mt-3">
                                 <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
@@ -159,36 +166,16 @@
                         </div>
                     </div>
 
-                    <div class="col-lg-2 col-md-6 footer-links">
+                    <div class="col-lg-5 col-md-6 footer-links">
                         <h4>Useful Links</h4>
                         <ul>
-                            <li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
-                            <li><i class="bx bx-chevron-right"></i> <a href="#">About us</a></li>
-                            <li><i class="bx bx-chevron-right"></i> <a href="#">Services</a></li>
-                            <li><i class="bx bx-chevron-right"></i> <a href="#">Terms of service</a></li>
-                            <li><i class="bx bx-chevron-right"></i> <a href="#">Privacy policy</a></li>
+                            <li><i class="bx bx-chevron-right"></i> <a href="#">Profil</a></li>
+                            <li><i class="bx bx-chevron-right"></i> <a href="#">Tentang</a></li>
+                            <li><i class="bx bx-chevron-right"></i> <a href="#">UMKM</a></li>
                         </ul>
                     </div>
 
-                    <div class="col-lg-3 col-md-6 footer-links">
-                        <h4>Our Services</h4>
-                        <ul>
-                            <li><i class="bx bx-chevron-right"></i> <a href="#">Web Design</a></li>
-                            <li><i class="bx bx-chevron-right"></i> <a href="#">Web Development</a></li>
-                            <li><i class="bx bx-chevron-right"></i> <a href="#">Product Management</a></li>
-                            <li><i class="bx bx-chevron-right"></i> <a href="#">Marketing</a></li>
-                            <li><i class="bx bx-chevron-right"></i> <a href="#">Graphic Design</a></li>
-                        </ul>
-                    </div>
 
-                    <div class="col-lg-4 col-md-6 footer-newsletter">
-                        <h4>Our Newsletter</h4>
-                        <p>Tamen quem nulla quae legam multos aute sint culpa legam noster magna</p>
-                        <form action="" method="post">
-                            <input type="email" name="email"><input type="submit" value="Subscribe">
-                        </form>
-
-                    </div>
 
                 </div>
             </div>
@@ -196,18 +183,11 @@
 
         <div class="container">
             <div class="copyright">
-                &copy; Copyright <strong><span>Scaffold</span></strong>. All Rights Reserved
+                &copy; Copyright <strong><span>HIPMIKINDO</span></strong>. All Rights Reserved
             </div>
-            <div class="credits">
-                <!-- All the links in the footer should remain intact. -->
-                <!-- You can delete the links only if you purchased the pro version. -->
-                <!-- Licensing information: https://bootstrapmade.com/license/ -->
-                <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/scaffold-bootstrap-metro-style-template/ -->
-                Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
-            </div>
+
         </div>
     </footer><!-- End Footer -->
-
     <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
             class="bi bi-arrow-up-short"></i></a>
 
